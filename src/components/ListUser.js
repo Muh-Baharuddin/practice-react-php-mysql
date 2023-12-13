6b5b0a01bab3aab8893795be6e676657a2a862f1
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-12-13 15:48:27 +0700

    create form

--- src/components/ListUser.js
+++ src/components/ListUser.js
@@ -1,5 +1,63 @@
+import { useState } from "react";
+
 export default function ListUser() {
+
+  const [input, setInput] = useState({})
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(input)
+  }
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInput(values => ({
+      ...values, 
+      [name]: value}))
+  }
+
   return (
-    <h1>List Users</h1>
+    <div>
+      <h1>List Users</h1>
+      <form onSubmit={handleSubmit}>
+        <table cellSpacing={"10"}>
+          <tbody>
+            <tr>
+              <th>
+                <label>Name: </label>
+              </th>
+              <td>
+                <input type="text" name="name" onChange={handleChange}/>
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label label>Email: </label>
+              </th>
+              <td>
+                <input type="email" name="email" onChange={handleChange}/>
+              </td>
+            </tr>
+
+            <tr>
+              <th>
+                <label>Mobile: </label>
+              </th>
+              <td>
+                <input type="number" name="mobile" onChange={handleChange}/>
+              </td>
+            </tr>
+
+            <tr>
+              <td colSpan={2} align="right">
+                <button>Save</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </div>
   )
 }
